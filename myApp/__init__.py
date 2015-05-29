4d4f64eed9f0336e77667b294639bec38a017a65
DMSeo <dmseo@ateamventures.com>
2015-05-29 16:34:14 +0900

    rm sqlAlchemy

--- myApp/__init__.py
+++ myApp/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sCloud:sCloudBiz@localhost/scloud'
-db = SQLAlchemy(app)
 
 @app.errorhandler(404)
 def not_found(error) :
